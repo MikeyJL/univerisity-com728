a2c1a52d29ac9e13dabc230987e6df115f4859f5
Mikey Lau <mikey.lau@traininblocks.com>
2021-10-31 21:43:16 +0000

    Get all records with or without suppliers

--- persistence/joins/database.py
+++ persistence/joins/database.py
@@ -16,7 +16,7 @@ def display_products_with_stock_levels ():
 def display_product_supplier():
     db = sq.connect('./catalogue.db')
     cur = db.cursor()
-    sql = "SELECT product.name, supplier.name FROM product INNER JOIN supplier ON product.supplier_id == supplier.id ;"
+    sql = "SELECT product.name, supplier.name FROM product INNER JOIN supplier ON product.supplier_id = supplier.id ;"
     cur.execute(sql)
     records = cur.fetchall()
 
@@ -31,13 +31,28 @@ def display_product_supplier_locations():
     cur = db.cursor()
     sql = "SELECT product.name, supplier.name, location.city, location.country " \
           "FROM product " \
-          "INNER JOIN supplier ON product.supplier_id == supplier.id " \
-          "INNER JOIN location ON supplier.location_id == location.id"
+          "INNER JOIN supplier ON product.supplier_id = supplier.id " \
+          "INNER JOIN location ON supplier.location_id = location.id"
     cur.execute(sql)
     records = cur.fetchall()
 
     for record in records:
         product, supplier, city, country = record
         print("\nProduct:", product, "\nSupplier:", supplier, "\nSupplier Location:", f"{city}, {country}")
+    
+    db.close()
+
+def display_products_missing_suppliers():
+    db = sq.connect('./catalogue.db')
+    cur = db.cursor()
+    sql = "SELECT product.name, supplier.name " \
+          "FROM product " \
+          "LEFT JOIN supplier ON product.supplier_id = supplier.id "
+    cur.execute(sql)
+    records = cur.fetchall()
+
+    for record in records:
+        product, supplier = record
+        print("\nProduct:", product, "\nSupplier:", supplier)
     
     db.close()
--- persistence/joins/main.py
+++ persistence/joins/main.py
@@ -1,10 +1,11 @@
-from database import display_products_with_stock_levels, display_product_supplier, display_product_supplier_locations
+import database as db
 
 def display_menu():
     menu = """\nPlease select one of the following options:
 [1] Display stock levels
 [2] Display suppliers
-[3] Display supplier locations"""
+[3] Display supplier locations
+[4] Display missing suppliers"""
     print(menu)
 
 def run():
@@ -18,11 +19,13 @@ def run():
     
     print("\nYou've selected option: ", option)
     if option == 1:
-        display_products_with_stock_levels()
+        db.display_products_with_stock_levels()
     elif option == 2:
-        display_product_supplier()
+        db.display_product_supplier()
     elif option == 3:
-        display_product_supplier_locations()
+        db.display_product_supplier_locations()
+    elif option == 4:
+        db.display_products_missing_suppliers()
 
 if __name__ == "__main__":
     run()
