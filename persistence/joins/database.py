2cd3bae4cc9844703c23d5ca3f52a1267b604c26
Mikey Lau <mikey.lau@traininblocks.com>
2021-10-31 18:59:22 +0000

    Added code to retrieve data using an inner join.

--- persistence/joins/database.py
+++ persistence/joins/database.py
@@ -12,3 +12,16 @@ def display_products_with_stock_levels ():
         print("\nProduct:", product, "\nDescription:", description, "\nStock:", stock)
     
     db.close()
+
+def display_product_supplier():
+    db = sq.connect('./catalogue.db')
+    cur = db.cursor()
+    sql = "SELECT product.name, supplier.name FROM product INNER JOIN supplier ON product.supplier_id == supplier.id ;"
+    cur.execute(sql)
+    records = cur.fetchall()
+
+    for record in records:
+        product, supplier = record
+        print("\nProduct:", product, "\nSupplier:", supplier)
+    
+    db.close()
--- persistence/joins/main.py
+++ persistence/joins/main.py
@@ -1,9 +1,9 @@
-from database import display_products_with_stock_levels
+from database import display_products_with_stock_levels, display_product_supplier
 
 def display_menu():
     menu = """\nPlease select one of the following options:
 [1] Display stock levels
-"""
+[2] Display suppliers"""
     print(menu)
 
 def run():
@@ -18,6 +18,8 @@ def run():
     print("\nYou've selected option: ", option)
     if option == 1:
         display_products_with_stock_levels()
+    elif option == 2:
+        display_product_supplier()
 
 if __name__ == "__main__":
     run()
