f173244a6c789f29dd19bdf97167d74cc2a8f19d
Mikey Lau <mikey.lau@traininblocks.com>
2021-11-07 17:18:40 +0000

    Added menu and validation

--- persistence/creation/main.py
+++ persistence/creation/main.py
@@ -1,3 +1,21 @@
 import database as db
 
 db.setup()
+
+menu = """
+Please select one of the following options:
+
+[1] Display presenters
+[2] Display events
+[3] Display presenters for event
+[4] Display events for presenter
+
+>>> """
+option = None
+while not option:
+    try:
+        option = int(input(menu))
+    except ValueError as e:
+        print(f"\n--------------------\n\nERROR: Please enter a number: {e}")
+
+print(f"\nYou've selected option {option}")
